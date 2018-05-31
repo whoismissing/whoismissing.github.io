427adc0170358831fe6346b92691adb5087b8508
Jason Chan <docholme@jasons-mbp.kitmews.ddns.net>
2018-05-31 18:53:51 -0500

    Finished banner and added favicon-- fonts in progress

--- assets/js/typewrite.js
+++ assets/js/typewrite.js
@@ -1,6 +1,6 @@
 // Credits to https://www.w3schools.com/howto/howto_js_typewriter.asp
 var i = 0;
-var txt = "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia cum minus sit beatae unde quod quo id repudiandae maiores, aperiam saepe inventore, cupiditate dolore soluta perspiciatis, numquam facilis. Alias, rerum.";
+var txt = "Welcome to my little corner of the internet where I've curated resources that I've found to be useful and documented things I've learned on my journey on the path of InfoSec.";
 var speed = 30;
 
 function typeWriter() {
